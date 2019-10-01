7df3d8efdeea41410932d8ae2eb63098bf13ef5e
Sharik Gurung <SGurung@spartaglobal.com>
2019-10-01 16:55:07 +0100

    pseudo code added for work

--- package_requests_post_codes.py
+++ package_requests_post_codes.py
@@ -1,5 +1,15 @@
 import requests
-
+# import json
 request_bbc = requests.get('https://www.bbc.co.uk/')
 
 print(request_bbc.content)
+
+# explore this request package that is installed
+# explore http://postcodes.io/
+
+#use requests to make a GET request to http://postcodes.io/ api
+# search for a post code (can be your own or any)
+
+# save response in variable
+
+# Use JSON library to unpack it
