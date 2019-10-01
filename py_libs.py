927d00f06a39638d867e5210397312650635adb4
Sharik Gurung <SGurung@spartaglobal.com>
2019-10-01 15:35:56 +0100

    maths and random class used

---
+++ py_libs.py
@@ -0,0 +1,23 @@
+# import random
+from random import random, randrange # Considered best practice
+# from random import * # If you want all the methods inside random
+import math
+import time
+
+print(random()) # this is an OOP
+print(randrange(10, 44))
+
+# libraries
+# They come built in
+# you just need to import them
+
+# they are maintained by python organisation
+# they are very stable and considered vanilla python
+# check the documentation for more
+# https://docs.python.org/3/library/random.html
+num = 24.64
+print(math.ceil(num))
+time.sleep(0.6)
+print(math.floor(num))
+time.sleep(0.31)
+print(math.pi)
